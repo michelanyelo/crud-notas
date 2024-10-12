3828bb3e34fd9eb3424fe84434c0fbff0c2de003
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-11 21:50:34 -0300

    Added FooterComp to App.vue and wrapped NavComp, RouterView, and FooterComp in a div with flexbox layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavComp from './components/app/NavComp.vue';
+import FooterComp from './components/app/FooterComp.vue';
 </script>
 
 <template>
 
-  <NavComp />
+  <div class="min-vh-100 d-flex flex-column">
+    <NavComp />
 
-  <RouterView />
+    <RouterView class="flex-fill" />
+
+    <FooterComp />
+  </div>
+
 </template>
 
 <style scoped></style>
---
+++ src/components/app/FooterComp.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        <div class="container">
+            <p class="text-center">
+                &copy; 2024 Administrador de Notas - All rights reserved.
+            </p>
+        </div>
+    </footer>
+</template>
+
+<style scoped></style>
