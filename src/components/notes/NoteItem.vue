ef8d5b8ef02ef9c093722ab91747c7fb14d39256
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-11 22:06:02 -0300

    Added NoteItem component import and usage in NotasView.vue

---
+++ src/components/notes/NoteItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { note } = defineProps({
+    note: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        <div class="card" style="width: 20rem;">
+            <div class="card-header">{{ note.title }}</div>
+            <div class="card-body">
+                <p class="card-text">{{ note.content }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/NotasView.vue
+++ src/views/NotasView.vue
@@ -1,10 +1,12 @@
 <script setup>
+import NoteItem from '@/components/notes/NoteItem.vue';
 
 </script>
 
 <template>
     <div>
         <h1>Estas son mis notas</h1>
+        <NoteItem :note="{ title: 'nota de prueba', content: 'contenido de prueba' }" />
     </div>
 </template>
 
