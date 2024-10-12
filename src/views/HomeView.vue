227282c705bbac65223d0719b0591df1535f0596
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-12 12:40:25 -0300

    Update HomeView.vue: changed header text, added paragraph and RouterLink

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,9 @@
 
 <template>
   <main>
-    <h1>Home View</h1>
+    <h1>Administrador de notas</h1>
+    <p>Aqui podras gestionar tus notas, apuntes, etc</p>
+    <RouterLink :to="{ name: 'notas' }" class="btn btn-info">Ver mis notas</RouterLink>
 
   </main>
 </template>
