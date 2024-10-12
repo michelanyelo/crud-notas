0d7b357c5c0fd4cf04892ec5d6af839dbd9f5a0d
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-12 12:50:18 -0300

    Update NoteFormView.vue to import and render NoteForm component

---
+++ src/components/notes/NoteForm.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+const noteData = ref({
+    title: "",
+    content: ""
+})
+
+const handleSubmit = () => {
+    alert("submit")
+}
+
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <div class="mb-3">
+                <label for="title">Titulo</label>
+                <input type="text" id="title" placeholder="Titulo de la nota" required v-model="noteData.title">
+            </div>
+
+            <div class="mb-3">
+                <label for="content">Contenido</label>
+                <textarea id="content" placeholder="Contenido de la nota" required
+                    v-model="noteData.content"></textarea>
+            </div>
+
+            <button class="btn btn-success" type="submit">Publicar</button>
+        </form>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/NoteFormView.vue
+++ src/views/NoteFormView.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+import NoteForm from '@/components/notes/NoteForm.vue';
 </script>
 
 <template>
     <div>
         <h1> Aqui podrás crear o editar una nota </h1>
+        <NoteForm />
     </div>
 </template>
 
