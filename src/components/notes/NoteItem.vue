69ca2ae278d25b5e04099e832e2656ee9a323993
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-12 18:44:16 -0300

    Update styles: modify variables, add new variables, and update Bootstrap overrides; refactor NoteItem component to use new styles

--- src/components/notes/NoteItem.vue
+++ src/components/notes/NoteItem.vue
@@ -13,8 +13,8 @@ const notesStore = useNotesStore()
 
 <template>
     <div>
-        <div class="card" style="width: 20rem;">
-            <div class="card-header d-flex" data-test="note-title">
+        <div class="note-item">
+            <div class="note-item__header d-flex" data-test="note-title">
                 <h3 class="flex-fill fw-bold fs-3">{{ note.title }}</h3>
                 <div class="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
@@ -35,11 +35,29 @@ const notesStore = useNotesStore()
                     </ul>
                 </div>
             </div>
-            <div class="card-body">
-                <p class="card-text" data-test="note-content">{{ note.content }}</p>
+            <div class="note-item__body">
+                <p class="note-item__content" data-test="note-content">{{ note.content }}</p>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.note-item {
+    border: 1px solid $color-light;
+    box-shadow: 0 0 10px $color-dark;
+    background-color: $color-light;
+    border-radius: $main-border-radius;
+
+    &__header {
+        display: flex;
+        background-color: $color-main-bg;
+        padding: .3rem;
+        border-radius: $main-border-radius $main-border-radius 0 0;
+    }
+
+    &__body {
+        padding: 2rem 1rem;
+    }
+}
+</style>
